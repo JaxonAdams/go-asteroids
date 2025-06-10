08fe22f9d1019b0e04cdd0490955133a967d6ed4
Jaxon Adams <jadamsresume1@gmail.com>
2025-06-10 16:24:43 -0600

    Add drawShape function and draw the ship

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+func drawShape(pos rl.Vector2, scale float32, points []rl.Vector2) {
+	transformer := func(point rl.Vector2) rl.Vector2 {
+		return rl.Vector2Add(rl.Vector2Scale(point, scale), pos)
+	}
+
+	for i := range len(points) {
+		current := transformer(points[i])
+		next := transformer(points[(i+1)%len(points)])
+
+		rl.DrawLineV(current, next, rl.White)
+	}
+}
 
 func main() {
 	rl.InitWindow(800, 450, "ASTEROIDS")
@@ -14,11 +29,17 @@ func main() {
 		rl.ClearBackground(rl.Black)
 
 		// Player Ship
-		rl.DrawLine(100, 300, 150, 200, rl.White)
-		rl.DrawLine(150, 200, 200, 300, rl.White)
-		rl.DrawLine(100, 300, 120, 280, rl.White)
-		rl.DrawLine(200, 300, 180, 280, rl.White)
-		rl.DrawLine(120, 280, 180, 280, rl.White)
+		drawShape(
+			rl.Vector2{X: 100, Y: 100},
+			40,
+			[]rl.Vector2{
+				{X: 0.2, Y: 0.8},
+				{X: 0.5, Y: 0.2},
+				{X: 0.8, Y: 0.8},
+				{X: 0.6, Y: 0.7},
+				{X: 0.4, Y: 0.7},
+			},
+		)
 
 		rl.EndDrawing()
 	}
