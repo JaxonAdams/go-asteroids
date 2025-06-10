65a3cf7ac0723e5fc493d18ff8e869992515d848
Jaxon Adams <jadamsresume1@gmail.com>
2025-06-10 16:59:33 -0600

    Apply rotation to the player ship

--- main.go
+++ main.go
@@ -9,12 +9,26 @@ const WINDOW_SIZE_Y = 450
 const SCALE = 40
 
 type Player struct {
-	pos rl.Vector2
+	pos   rl.Vector2
+	shape []rl.Vector2
 }
 
-func drawShape(pos rl.Vector2, scale float32, points []rl.Vector2) {
+func computeCentroid(points []rl.Vector2) rl.Vector2 {
+	var sum rl.Vector2
+	for _, p := range points {
+		sum = rl.Vector2Add(sum, p)
+	}
+	return rl.Vector2Scale(sum, 1/float32(len(points)))
+}
+
+func drawShape(pos rl.Vector2, scale float32, rotation float32, points []rl.Vector2) {
+	centroid := computeCentroid(points)
+
 	transformer := func(point rl.Vector2) rl.Vector2 {
-		return rl.Vector2Add(rl.Vector2Scale(point, scale), pos)
+		centered := rl.Vector2Subtract(point, centroid)
+		scaled := rl.Vector2Scale(centered, scale)
+		rotated := rl.Vector2Rotate(scaled, rotation)
+		return rl.Vector2Add(rotated, pos)
 	}
 
 	for i := range len(points) {
@@ -25,13 +39,6 @@ func drawShape(pos rl.Vector2, scale float32, points []rl.Vector2) {
 	}
 }
 
-func centerShape(startPos rl.Vector2) rl.Vector2 {
-	return rl.Vector2Subtract(
-		startPos,
-		rl.Vector2{X: SCALE, Y: SCALE},
-	)
-}
-
 func main() {
 	rl.InitWindow(WINDOW_SIZE_X, WINDOW_SIZE_Y, "ASTEROIDS")
 	defer rl.CloseWindow()
@@ -39,13 +46,22 @@ func main() {
 	rl.SetTargetFPS(60)
 
 	player := Player{
-		pos: centerShape(
-			rl.Vector2Scale(rl.Vector2{
-				X: WINDOW_SIZE_X,
-				Y: WINDOW_SIZE_Y,
-			}, 0.5),
-		),
+		shape: []rl.Vector2{
+			{X: 0.2, Y: 0.8},
+			{X: 0.5, Y: 0.2},
+			{X: 0.8, Y: 0.8},
+			{X: 0.6, Y: 0.7},
+			{X: 0.4, Y: 0.7},
+		},
 	}
+
+	player.pos = rl.Vector2Add(
+		rl.Vector2Scale(rl.Vector2{
+			X: WINDOW_SIZE_X,
+			Y: WINDOW_SIZE_Y,
+		}, 0.5),
+		computeCentroid(player.shape),
+	)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
@@ -56,13 +72,8 @@ func main() {
 		drawShape(
 			player.pos,
 			SCALE,
-			[]rl.Vector2{
-				{X: 0.2, Y: 0.8},
-				{X: 0.5, Y: 0.2},
-				{X: 0.8, Y: 0.8},
-				{X: 0.6, Y: 0.7},
-				{X: 0.4, Y: 0.7},
-			},
+			float32(rl.GetTime())*rl.Pi,
+			player.shape,
 		)
 
 		rl.EndDrawing()
